e021002f14e739e79cf8e24d64ea5b19479b80d1
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2024-12-24 21:28:42 +0700

    Updated

--- NoteApp/app/src/main/java/thick/nguyenbuiquanghuy/noteapp/SplashActivity.java
+++ NoteApp/app/src/main/java/thick/nguyenbuiquanghuy/noteapp/SplashActivity.java
@@ -1,12 +1,12 @@
 package thick.nguyenbuiquanghuy.noteapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -15,10 +15,13 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
