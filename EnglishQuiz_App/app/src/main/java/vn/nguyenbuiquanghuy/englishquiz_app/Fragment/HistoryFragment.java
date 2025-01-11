e98fbcfdedf08a01529ee37e2e8d0c4b07157cca
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-01-11 22:06:37 +0700

    Updated

--- EnglishQuiz_App/app/src/main/java/vn/nguyenbuiquanghuy/englishquiz_app/Fragment/HistoryFragment.java
+++ EnglishQuiz_App/app/src/main/java/vn/nguyenbuiquanghuy/englishquiz_app/Fragment/HistoryFragment.java
@@ -28,7 +28,7 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
         rvHistory = view.findViewById(R.id.rv_history);
         rvHistory.setLayoutManager(new LinearLayoutManager(getActivity()));
         dbHelper = new HistoryDatabase(getActivity());
--- EnglishQuiz_App/app/src/main/java/vn/nguyenbuiquanghuy/englishquiz_app/Fragment/SettingFragment.java
+++ EnglishQuiz_App/app/src/main/java/vn/nguyenbuiquanghuy/englishquiz_app/Fragment/SettingFragment.java
@@ -1,21 +1,16 @@
 package vn.nguyenbuiquanghuy.englishquiz_app.Fragment;
 
 import static android.content.Context.MODE_PRIVATE;
-
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import vn.nguyenbuiquanghuy.englishquiz_app.R;
-
 
 public class SettingFragment extends Fragment {
     EditText edTime, edQues;
@@ -35,7 +30,6 @@ public class SettingFragment extends Fragment {
 
         loadSettings();
 
-        // Thiết lập sự kiện cho nút giảm thời gian
         btnTimeMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,7 +37,6 @@ public class SettingFragment extends Fragment {
             }
         });
 
-        // Thiết lập sự kiện cho nút tăng thời gian
         btnTimePlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,7 +44,6 @@ public class SettingFragment extends Fragment {
             }
         });
 
-        // Thiết lập sự kiện cho nút giảm số câu hỏi
         btnQuesMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,7 +51,6 @@ public class SettingFragment extends Fragment {
             }
         });
 
-        // Thiết lập sự kiện cho nút tăng số câu hỏi
         btnQuesPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,7 +58,6 @@ public class SettingFragment extends Fragment {
             }
         });
 
-        // Thiết lập sự kiện lưu cài đặt
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,7 +67,6 @@ public class SettingFragment extends Fragment {
         return view;
     }
 
-    // Phương thức để thay đổi giá trị
     private void changeValue(EditText editText, int change) {
         try {
             int currentValue = Integer.parseInt(editText.getText().toString());
@@ -85,30 +74,28 @@ public class SettingFragment extends Fragment {
             if (newValue < 1) newValue = 1;
             editText.setText(String.valueOf(newValue));
         } catch (NumberFormatException e) {
-            Toast.makeText(getActivity(), "Vui lòng nhập số hợp lệ", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(),
+                    "Please enter a valid number", Toast.LENGTH_SHORT).show();
         }
     }
-
     private void loadSettings() {
         SharedPreferences preferences = getContext().getSharedPreferences("QuizSettings", MODE_PRIVATE);
-        int time = preferences.getInt("timeLimit", 30);  // Giá trị mặc định là 30 giây
-        int questions = preferences.getInt("numberOfQuestions", 10);  // Giá trị mặc định là 10 câu hỏi
+        int time = preferences.getInt("timeLimit", 30);
+        int questions = preferences.getInt("numberOfQuestions", 10);
 
         edTime.setText(String.valueOf(time));
         edQues.setText(String.valueOf(questions));
     }
 
-    // Phương thức lưu cài đặt
     private void saveSettings() {
         String time = edTime.getText().toString();
         String questions = edQues.getText().toString();
         SharedPreferences preferences = getContext().getSharedPreferences("QuizSettings", MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putInt("timeLimit", Integer.parseInt(time));  // Thời gian trong giây
-        editor.putInt("numberOfQuestions", Integer.parseInt(questions));  // Số câu hỏi
-        editor.apply();  // Lưu thay đổi
-
-        Toast.makeText(getActivity(), "Cài đặt đã được lưu\nThời gian: " + time + " giây\nSố câu hỏi: " + questions, Toast.LENGTH_SHORT).show();
+        editor.putInt("timeLimit", Integer.parseInt(time));
+        editor.putInt("numberOfQuestions", Integer.parseInt(questions));
+        editor.apply();
+        Toast.makeText(getActivity(), "Setting is saved", Toast.LENGTH_SHORT).show();
     }
 
 }
